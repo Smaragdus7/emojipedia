75ce06e86e341426ac7842e7afbc8b073ab3be99
Claudia Ibarra <Smaragdus7@users.noreply.github.com>
2023-03-14 11:52:53 -0600

    Arrow function update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,22 @@ import React from "react";
 import Entry from "./Entry";
 import emojis from "../emojipedia";
 
-function newEntry(emoji) {
-  return (
-    <Entry
-      key={emoji.id}
-      emoji={emoji.emoji}
-      name={emoji.name}
-      meaning={emoji.meaning}
-    />
-  );
-}
-
 function App() {
   return (
     <div>
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{emojis.map(newEntry)}</dl>
+      <dl className="dictionary">
+        {emojis.map((emoji) => (
+          <Entry
+            key={emoji.id}
+            emoji={emoji.emoji}
+            name={emoji.name}
+            meaning={emoji.meaning}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
